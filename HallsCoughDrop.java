750ba31041172b7d045163a909b6cfe1288a5545
LucasWeiner <LucasWeiner96@gmail.com>
2014-10-29 23:44:56 -0400

    Hall

---
+++ HallsCoughDrop.java
@@ -0,0 +1,9 @@
+public class HallsCoughDrop extends Enemy {
+	public HallsCoughDrop () {
+		name = "HallsCoughDrop";
+		maxHP += 50.0;
+		HP += 50.0
+		damage += 20.0;
+		multiplier += .25;
+		}
+		}
