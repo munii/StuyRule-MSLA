11acc916af40a724ad971c905a989e6979ec0592
LucasWeiner <LucasWeiner96@gmail.com>
2014-10-29 23:47:53 -0400

    Heli

---
+++ Helicopter.java
@@ -0,0 +1,10 @@
+public class Helicopter extends Enemy {
+	public Helicopter () {
+		name = "Helicopter";
+		maxHP += 25.0;
+		HP += 25.0
+		damage += 20.0;
+		multiplier += .5;
+		speed += 15;
+		}
+		}
