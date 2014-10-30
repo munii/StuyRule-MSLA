87ece0eae7dc311321a522b3367212ac7d0502da
LucasWeiner <LucasWeiner96@gmail.com>
2014-10-29 23:51:14 -0400

    Vomit

---
+++ VomitPile.java
@@ -0,0 +1,11 @@
+public class VomitPile extends Enemy {
+	public VomitPile () {
+		name = "VomitPile";
+		maxHP;
+		HP;
+		defense += 5;
+		damage += 30.0;
+		multiplier += .75;
+		speed += 5;
+		}
+		}
