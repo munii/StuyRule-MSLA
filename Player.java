0e9e51014a9448648020c127bc91c93bfaf12942
LucasWeiner <LucasWeiner96@gmail.com>
2014-10-30 00:00:30 -0400

    non negs

--- Player.java
+++ Player.java
@@ -46,6 +46,7 @@ public class Player{
 	public void attack( Guy c ) {
 	Random rand = new Random();
 	if (rand.nextInt(100) + 1 <= accuracy){
+		if (damage * multiplier - c.defense > 0){
 	    if (c.HP - (damage * multiplier - c.defense) >= 0){
 		c.HP-=( damage * multiplier - c.defense);
 		System.out.println(this.name + " " + "dealt" + " " + ( damage * multiplier - c.defense) + " " + "points of damage to" + " " + c.name);
@@ -54,6 +55,7 @@ public class Player{
 	    	System.out.println(this.name + " " + "dealt" + " " + c.HP + " " + "points of damage to" + " " + c.name);
 		c.HP = 0;
 	    }
+	}
 	}
 	else{
 	    System.out.println(this.name + " " + "missed!");
@@ -90,8 +92,10 @@ public class Player{
 	    	System.out.println(this.name + " " + "dealt" + " " + e.HP + " " + "points of damage to" + " " + e.name);
 		e.HP = 0;
 	    }
-	    HP -= (damage * multiplier / 4 - defense);
+	    if (damage * multiplier / 4 - defense > 0){
+		HP -= (damage * multiplier / 4 - defense);
 	    System.out.println(this.name + "'s ferocious attack recoiled and did" + " " + (damage * multiplier / 4 - defense) + " " + "points of self inflicted damage (masochist)");
+	}
 	}
 	else{
 	    System.out.println(this.name + " " + "missed bad");
@@ -102,6 +106,7 @@ public class Player{
     public void strongAttack(Guy e){
 	Random rand = new Random();
 	if (rand.nextInt(100) + 1 <= accuracy - 10){
+		if ((damage + .5) * multiplier - e.defense > 0)
 	    if (e.HP - ((damage + .5) * multiplier - e.defense) >= 0){
 		e.HP-=( (damage + .5) * multiplier - e.defense);
 		System.out.println(this.name + " " + "dealt" + " " + ( (damage + .5) * multiplier - e.defense) + " " + "points of damage to" + " " + e.name);
