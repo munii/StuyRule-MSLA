5c5ae44078e99a706ce6c033062e0c7b3be613b4
LucasWeiner <LucasWeiner96@gmail.com>
2014-10-29 23:49:37 -0400

    Libmon

---
+++ LibraryMonitor.java
@@ -0,0 +1,11 @@
+public class Helicopter extends Enemy {
+	public Helicopter () {
+		name = "Helicopter";
+		maxHP += 75.0;
+		HP += 75.0;
+		defense += 20;
+		damage += 10.0;
+		multiplier += .5;
+		speed -= 1;
+		}
+		}
